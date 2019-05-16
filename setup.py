b72d946a8224216b27a83908dfd82410988ccb5a
Konrad Mohrfeldt <mohrfeldt@silicann.com>
2019-05-16 14:47:34 +0200

    library only has python3 compatibility

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
